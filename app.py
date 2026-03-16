8d994b91f5e03c8ac967321dccce3a6985280fa9
Vardhanraiz <167404535+Vardhanraiz@users.noreply.github.com>
2026-03-16 22:44:04 +0530

    Add user authentication to the Streamlit app
    Implement user authentication using Streamlit and YAML configuration.

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+with open("database/users.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"]
+)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username or password is incorrect")
+
+elif authentication_status == None:
+    st.warning("Please enter your username and password")
+
+elif authentication_status:
+
+    authenticator.logout("Logout", "sidebar")
+
+    st.sidebar.success(f"Welcome {name}")
+
+    st.title("Alzheimer AI System Dashboard")
