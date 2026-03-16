bdf81acea5799951ca017031b5d51f59236e4db2
Vardhanraiz <167404535+Vardhanraiz@users.noreply.github.com>
2026-03-16 22:48:48 +0530

    Configure page settings and enhance dashboard layout
    Added page configuration and updated dashboard layout with metrics.

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ if authentication_status == False:
 elif authentication_status == None:
     st.warning("Please enter your username and password")
 
+st.set_page_config(
+    page_title="Alzheimer AI Platform",
+    layout="wide"
+)
+
+
 elif authentication_status:
 
     authenticator.logout("Logout", "sidebar")
@@ -28,3 +34,37 @@ elif authentication_status:
     st.sidebar.success(f"Welcome {name}")
 
     st.title("Alzheimer AI System Dashboard")
+
+
+st.set_page_config(
+    page_title="Alzheimer AI Platform",
+    layout="wide"
+)
+page = st.sidebar.radio(
+    "Navigation",
+    [
+        "Dashboard",
+        "Upload MRI",
+        "Patient History",
+        "About System"
+    ]
+)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Total Scans", 120)
+
+with col2:
+    st.metric("Patients", 60)
+
+with col3:
+    st.metric("Model Accuracy", "74%")
+
+import matplotlib.pyplot as plt
+
+labels = ["Non", "Very Mild", "Mild", "Moderate"]
+
+plt.bar(labels, preds[0])
+
+st.pyplot(plt)
